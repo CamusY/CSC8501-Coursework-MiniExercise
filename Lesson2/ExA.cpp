03ba58c3561ec8d2b33149ff13e538ac7ae8b0f5
CamusY <chienranyu@gmail.com>
2025-09-22 14:26:56 +0100

    finished

--- Lesson2/ExA.cpp
+++ Lesson2/ExA.cpp
@@ -16,3 +16,26 @@ int main() {
     cout << "Average: " << avg << "  passed? " << (t ? "yes" : "no") << endl;
     return 0;
 }
+/*
+ variables which need to be removed or modified:
+ tmp - not used
+ avg - declared early, uninitialised
+ t - defaulted, then overwritten
+ a, b, c, total - could be declared closer to their use
+ if statement - could be replaced with a direct assignment using a boolean expression
+ final refactored code:
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+        int a = 78;        // exam 1
+        int b = 85;        // exam 2
+        int c = 92;        // exam 3
+        int total = a + b + c;
+        double avg = total / 3.0;
+        bool t = (avg >= 50.0);
+        cout << "Average: " << avg << "  passed? " << (t ? "yes" : "no") << endl;
+        return 0;
+    }
+
+*/
